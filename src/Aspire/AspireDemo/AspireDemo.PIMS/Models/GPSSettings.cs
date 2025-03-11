e5dfe571a13dd36c10895a7624cee45449371f54
marqdouj <16510416+marqdouj@users.noreply.github.com>
2025-03-11 05:46:59 -0600

    GPSSettings now derives from StateContainer

--- src/Aspire/AspireDemo/AspireDemo.PIMS/Models/GPSSettings.cs
+++ src/Aspire/AspireDemo/AspireDemo.PIMS/Models/GPSSettings.cs
@@ -1,10 +1,23 @@
 ﻿using AzureMapsControl.Components.Map;
+using Marqdouj.CLRCommon;
 
 namespace AspireDemo.PIMS.Models
 {
-    public class GPSSettings
+    public class GPSSettings : StateContainer
     {
-        public CameraOptions? MapCamera { get; set; }
-        public StyleOptions? MapStyle { get; set; }
+        #region MapCamera
+        private CameraOptions? _mapCamera;
+        public CameraOptions? MapCamera { get => _mapCamera; set => SetValue(ref _mapCamera, value); }
+        #endregion
+
+        #region MapStyle
+        private StyleOptions? _mapStyle;
+        public StyleOptions? MapStyle { get => _mapStyle; set => SetValue(ref _mapStyle, value); }
+        #endregion
+
+        #region ShowInstructions
+        private bool _showInstructions = true;
+        public bool ShowInstructions { get => _showInstructions; set => SetValue(ref _showInstructions, value); }
+        #endregion
     }
 }
